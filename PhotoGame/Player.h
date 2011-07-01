9885338bc20f9224605648f81547b964ce7412f0
Steve Ho <showrx@gmail.com>
2011-07-01 00:50:46 -0700

    ...

---
+++ PhotoGame/Player.h
@@ -0,0 +1,16 @@
+//
+//  Player.h
+//  PhotoGame
+//
+//  Created by Steve Ho on 7/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Player : NSObject {
+    
+}
+
+@end
---
+++ PhotoGame/Player.m
@@ -0,0 +1,14 @@
+//
+//  Player.m
+//  PhotoGame
+//
+//  Created by Steve Ho on 7/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Player.h"
+
+
+@implementation Player
+
+@end
